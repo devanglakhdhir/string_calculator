3e1a201dee2cb04bc1e7b50ecb6bb69c9e737f75
devanglakhdhir <devanglakhdhir@gmail.com>
2025-02-02 13:06:12 +0530

    [Update] - write delimiter logic on string calculator file

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,7 +1,6 @@
-class StringCalculator{
-  int add(String numbers){
-
-    if(numbers.isEmpty) return 0;
+class StringCalculator {
+  int add(String numbers) {
+    if (numbers.isEmpty) return 0;
     String delimiter = r'[,\n]';
     String numberString = numbers;
 
@@ -10,8 +9,20 @@ class StringCalculator{
       final parts = numbers.split('\n');
       final delimiterSection = parts.first.substring(2);
       numberString = parts.sublist(1).join('\n');
-      numbers = parts[1];
+
+      //support for multiple delimiters
+      final delimiters = RegExp(r'\[(.*?)\]')
+          .allMatches(delimiterSection)
+          .map((m) => RegExp.escape(m.group(1)!))
+          .toList();
+
+      if (delimiters.isEmpty) {
+        delimiters.add(RegExp.escape(delimiterSection));
+      }
+      delimiter = delimiters.join('|');
     }
+
+
     return int.parse(numbers);
   }
 }
