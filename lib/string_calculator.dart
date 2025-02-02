5e009c075705526c33fdfa674a40cfcbbeeb22e6
devanglakhdhir <devanglakhdhir@gmail.com>
2025-02-02 12:35:49 +0530

    [Update] - write logic for delimiters numbers and new line between number

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,7 +1,16 @@
 class StringCalculator{
   int add(String numbers){
-    if(numbers.isEmpty){
-      return 0;
+
+    if(numbers.isEmpty) return 0;
+    String delimiter = r'[,\n]';
+    String numberString = numbers;
+
+    // Check for custom delimiter
+    if (numbers.startsWith('//')) {
+      final parts = numbers.split('\n');
+      final delimiterSection = parts.first.substring(2);
+      numberString = parts.sublist(1).join('\n');
+      numbers = parts[1];
     }
     return int.parse(numbers);
   }
